396217a57d38af38eca204ffb11824d017b63280
iwillems <ischa.willems@gmail.com>
2026-06-13 22:02:23 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE = 'gymlog-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(resp => {
+        if (resp && resp.status === 200 && resp.type === 'basic') {
+          const clone = resp.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, clone));
+        }
+        return resp;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
